4edfbf04ed41f5c9d18c13f024e88b2b01280822
yag1Rice <yag1@rice.edu>
2026-06-30 17:21:50 -0400

    adding manim - Youssef

--- BackEnd/api.py
+++ BackEnd/api.py
@@ -139,7 +139,7 @@ def get_dummy_paths(target: str):
             return {"video_paths": {
                 "video_paths": [{"scene_id": "1", "video_path": str(BACKEND_DIR / "Video_Generation_Pipeline" / "output" / "demo" / "demo_scene_1.mp4")},
                                 {"scene_id": "2", "video_path": str(BACKEND_DIR / "Video_Generation_Pipeline" / "output" / "demo" / "demo_scene_2.mp4")}],
-                "manim_path": "",
+                "manim_path": str(BACKEND_DIR / "demo_manim_video" / "therapeutic_communication_combined.mp4"),
             }}
 
 

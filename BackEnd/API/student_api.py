16cb3d1b2d0a02646ba6565b2b22b83c0c874903
yag1Rice <yag1@rice.edu>
2026-07-15 16:22:21 -0400

    dummy endpoint - Youssef

--- BackEnd/API/student_api.py
+++ BackEnd/API/student_api.py
@@ -42,3 +42,37 @@ def get_assets(project_id: str):
         raise HTTPException(
             status_code=500, detail=f"Error retrieving assets: {str(e)}"
         )
+
+
+# This endpoint will be called by the frontend to retrieve the generated assets to display them in the frontend
+@student_router.get("/dummy_assets")
+def get_assets():
+    supabase_client = setup_supabase_client()
+
+    project_id = "test"
+
+    try:
+
+        video_list = supabase_client.storage.from_("Videos").list(project_id)
+
+        video_links = dict()
+
+        for video in video_list:
+            video_links[video["name"]] = supabase_client.storage.from_(
+                "Videos"
+            ).get_public_url(f"{project_id}/{video['name']}")
+
+        script_link = supabase_client.storage.from_("Scripts").get_public_url(
+            f"{project_id}/script.json"
+        )
+
+        return {
+            "message": "Assets retrieved successfully",
+            "script_link": script_link,
+            "video_links": video_links,
+        }
+
+    except StorageApiError as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error retrieving assets: {str(e)}"
+        )
